81dbd540faea951a045c7ae26d49b93c3d16c1b5
Samad Shaikh <47159226+asamadshaikh@users.noreply.github.com>
2021-06-23 22:16:31 -0600

    Update TestBankAccount.java

--- assignment3/BankAccount/TestBankAccount.java
+++ assignment3/BankAccount/TestBankAccount.java
@@ -9,7 +9,7 @@ public class TestBankAccount {
         //check default Construcotr
         System.out.println("Default Constructor");
         BankAccount bankAccount=new BankAccount();
-        System.out.println("Print detials\n"+bankAccount);
+        System.out.println("Print details\n"+bankAccount);
         bankAccount.setAccountNumber(123456);
         bankAccount.setAddress("Adress Check 1");
         bankAccount.setFirstName("First Name 1");
@@ -17,26 +17,26 @@ public class TestBankAccount {
         bankAccount.setBalance(100);
         bankAccount.deposit(200);
         bankAccount.withdraw(50);
-        System.out.println("Print detials\n"+bankAccount);
+        System.out.println("Print details\n"+bankAccount);
         
         //String parameter Constructor
         System.out.println("String Parameter Constructor");
         BankAccount bankAccount1=new BankAccount("firstName2", "lastName2", "address check 2");
-        System.out.println("Print detials\n"+bankAccount1);
+        System.out.println("Print details\n"+bankAccount1);
         bankAccount1.setAccountNumber(123457);
         bankAccount1.setBalance(150);
         bankAccount1.deposit(250);
         bankAccount1.withdraw(100);
-        System.out.println("Print detials\n"+bankAccount1);
+        System.out.println("Print details\n"+bankAccount1);
         
         
         //All Parameter Constructor
         System.out.println("All Parameter Constructor");
         BankAccount bankAccount2=new BankAccount(123458,"firstName3", "lastName3", "address check 3",100);
-        System.out.println("Print detials\n"+bankAccount2);
+        System.out.println("Print details\n"+bankAccount2);
         bankAccount2.deposit(250);
         bankAccount2.withdraw(100);
-        System.out.println("Print detials\n"+bankAccount2);
+        System.out.println("Print details\n"+bankAccount2);
         
     }
 }
